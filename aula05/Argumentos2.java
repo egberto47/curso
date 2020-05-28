b2786a54eb46f1be489183b90407b6560f82bf13
Egberto Ricardo de Oliveira <egberto47@gmail.com>
2020-05-28 16:22:13 -0300

    Alteracao nos Argumentos2

--- aula05/Argumentos2.java
+++ aula05/Argumentos2.java
@@ -2,5 +2,6 @@ class Argumentos2{
 	public static void main(String[] args){
 		System.out.println("Ola mundo!");
 		System.out.println("\nVoce digitou: " +args[0]);
+		System.out.println("\nDigite : " +args[0]);
 	}
 }
