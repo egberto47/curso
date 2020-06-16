c363ead81e12cc7746f5840a3d4963dd712d93b3
egberto47 <egberto47@gmail.com>
2020-06-16 12:58:28 -0300

    branch testes

--- aula05/Argumentos2.java
+++ aula05/Argumentos2.java
@@ -1,7 +1,7 @@
 class Argumentos2{
 	public static void main(String[] args){
-		System.out.println("Ola mundo!");
-		System.out.println("\nVoce digitou: " +args[0]);
-		System.out.println("\nDigite : " +args[0]);
+		System.out.println("\n Ola mundo!");
+		System.out.println("\n Voce digitou: " +args[0]);
+		System.out.println("\n Digite : " +args[0]);
 	}
 }
